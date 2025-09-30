99883576078d2e275795b948753187f12ad43a07
prashant007 <prashantgi1995@gmail.com>
2025-09-30 21:48:53 +0530

    updated sonar host url

---
+++ jest.config.cjs
@@ -0,0 +1,23 @@
+/** @type {import('jest').Config} */
+const config = {
+  roots: ['<rootDir>/src', '<rootDir>/__tests__'],
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  testEnvironment: 'jsdom',
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts',
+  ],
+  transform: {
+    '^.+\\.(ts|tsx|js|jsx)$': 'babel-jest'
+  },
+  moduleNameMapper: {
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy'
+  },
+  extensionsToTreatAsEsm: ['.ts', '.tsx', '.jsx'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+}
+
+module.exports = config
